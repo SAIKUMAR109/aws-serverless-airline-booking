9b9d909f93f6377ba73ce3c2250fe898ae261875
SAIKUMAR109 <140240280+SAIKUMAR109@users.noreply.github.com>
2024-12-21 04:03:05 +0530

    Create Booking controller.java

---
+++ Booking controller.java
@@ -0,0 +1,27 @@
+package com.example.airlinebooking.controller;
+
+import com.example.airlinebooking.model.Booking;
+import com.example.airlinebooking.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+
+    @PostMapping
+    public Booking createBooking(@RequestBody Booking booking) {
+        return bookingRepository.save(booking);
+    }
+}
